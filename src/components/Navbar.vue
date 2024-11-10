ec83bdc99dee3715116e86c878c212784c22b8db
Steven Chow <stevenchow33@hotmail.com>
2024-11-10 15:10:06 +0800

    adding files for first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,6 @@
+<template>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+    <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container>
+    <h1>Hello World</h1>
+    <v-btn color="primary">Click me</v-btn>
+  </v-container>
+</template>
